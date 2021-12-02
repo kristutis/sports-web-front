7ab5359684cf2b6b530fbc2fab19a93d7b6895eb
kristutis <kristijonas.karalaitis@gmail.com>
2021-12-02 23:04:19 +0200

    added login modal

--- src/components/modal/LoginModal.jsx
+++ src/components/modal/LoginModal.jsx
@@ -4,26 +4,24 @@ import './LoginModal.css'
 function LoginModal() {
     return (
         <>
-            <div class="login-modal-form login-modal-body">
-                <div class="login-modal-title">Welcome</div>
-                <div class="login-modal-subtitle">Let's create your account!</div>
-                <div class="login-modal-input-container ic1">
-                    <input id="firstname" class="login-modal-input" type="text" placeholder=" " />
-                    <div class="login-modal-cut"></div>
-                    <label for="firstname" class="login-modal-placeholder">First name</label>
-                </div>
-                <div class="login-modal-input-container ic2">
-                    <input id="lastname" class="login-modal-input" type="text" placeholder=" " />
-                    <div class="login-modal-cut"></div>
-                    <label for="lastname" class="login-modal-placeholder">Last name</label>
-                </div>
-                <div class="login-modal-input-container ic2">
-                    <input id="email" class="login-modal-input" type="text" placeholder=" " />
-                    <div class="login-modal-cut cut-short"></div>
-                    <label for="email" class="login-modal-placeholder">Email</label>
-                </div>
-                <button type="text" class="login-modal-submit">submit</button>
-            </div>
+            <div class="login">
+  <h1>Login to Web App</h1>
+  <form method="post" action="">
+    <p><input type="text" name="login" value="" placeholder="Username or Email"/></p>
+    <p><input type="password" name="password" value="" placeholder="Password"/></p>
+    <p class="remember_me">
+      <label>
+        <input type="checkbox" name="remember_me" id="remember_me"/>
+        Remember me on this computer
+      </label>
+    </p>
+    <p class="submit"><input type="submit" name="commit" value="Login"/></p>
+  </form>
+</div>
+
+<div class="login-help">
+  <p>Forgot your password? <a href="#">Click here to reset it</a>.</p>
+</div>
         </>
     )
 }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LoginModal from "./LoginModal";
 import "./Modal.css";
 
 export default function Modal(props) {
@@ -19,14 +20,7 @@ export default function Modal(props) {
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
             <h2>Hello Modal</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-              perferendis suscipit officia recusandae, eveniet quaerat assumenda
-              id fugit, dignissimos maxime non natus placeat illo iusto!
-              Sapiente dolorum id maiores dolores? Illum pariatur possimus
-              quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
-              placeat tempora vitae enim incidunt porro fuga ea.
-            </p>
+            <LoginModal/>
             <button className="close-modal" onClick={toggleModal}>
               x
             </button>
