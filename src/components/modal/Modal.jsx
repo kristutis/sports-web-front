8414ce4f0291ccc8226682d2cabe3f0ff1cfc620
kristutis <kristijonas.karalaitis@gmail.com>
2021-12-02 23:20:25 +0200

    added login form

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -19,8 +19,8 @@ export default function Modal(props) {
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2>Hello Modal</h2>
-            <LoginModal/>
+            <h2>Please fill out form</h2>
+            {props.loginModal && <LoginModal/>}
             <button className="close-modal" onClick={toggleModal}>
               x
             </button>
