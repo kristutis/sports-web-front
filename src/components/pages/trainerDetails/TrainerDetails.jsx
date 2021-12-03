538f199e30029b065829d8d9e4d17808a26d3d3e
kristutis <kristijonas.karalaitis@gmail.com>
2021-12-03 23:57:41 +0200

    fetched details data

--- src/components/pages/trainerDetails/TrainerDetails.jsx
+++ src/components/pages/trainerDetails/TrainerDetails.jsx
@@ -1,14 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useParams } from "react-router-dom";
+import { DEFAULT_BACKEND_PATH } from '../../../App';
 
 function TrainerDetails() {
+    console.log('hello')
     const params = useParams();
+    const trainerId = params.id
+
+    const [trainer, setTrainer] = useState(null)
+    const [ratings, setRatings] = useState(null)
+    const [ratingAverage, setratingAverage] = useState(null)
+    const [comments, setComments] = useState(null)
+
+    useEffect(() => {
+        fetch(DEFAULT_BACKEND_PATH + 'trainers/' + trainerId)
+            .then(response => response.json())
+            .then(trainer => {
+                const trainerData = Object.values(trainer).map(key => {
+                    return (
+                        <p>{key}</p>
+                    )
+                })
+                setTrainer(trainerData)
+            })
+            .catch(e => console.log(e))
+
+        fetch(DEFAULT_BACKEND_PATH + 'trainers/' + trainerId + '/comments')
+            .then(response => response.json())
+            .then(commentsArray => {
+                const comms = commentsArray.map(comment => {
+                    return (
+                        <ul>
+                            {Object.values(comment).map(value => {
+                                return (
+                                    <li>{value}</li>
+                                )
+                            })}
+                        </ul>
+                    )
+                })
+                setComments(comms)
+            })
+            .catch(e => console.log(e))
+
+        fetch(DEFAULT_BACKEND_PATH + 'trainers/' + trainerId + '/ratings')
+            .then(response => response.json())
+            .then(ratingsArray => {
+                const comms = ratingsArray.map(rating => {
+                    return (
+                        <ul>
+                            {Object.values(rating).map(value => {
+                                return (
+                                    <li>{value}</li>
+                                )
+                            })}
+                        </ul>
+                    )
+                })
+                setRatings(comms)
+            })
+            .catch(e => console.log(e))
+
+        fetch(DEFAULT_BACKEND_PATH + 'trainers/' + trainerId + '/ratings/average')
+            .then(response => response.json())
+            .then(average =>
+                setratingAverage(average)
+            )
+            .catch(e => console.log(e))
+    }, [])
 
     return (
-        <div>
-            <p>hello</p>
-            <p>{params.id}</p>
-        </div>
+        <>
+            {!!trainer && trainer}
+            <p>-------------------------------</p>
+            {!!comments && comments}
+            <p>-------------------------------</p>
+            {!!ratings && ratings}
+            <p>-------------------------------</p>
+            {!!ratingAverage && <p>{ratingAverage}</p>}
+        </>
     )
 }
 
